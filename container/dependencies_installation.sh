d50e6742ea9dcaa81e235a00b1ef946322d2e86c
allpaul0 <paulallaireperso@gmail.com>
2026-04-29 17:26:00 +0200

    add m-v2-develop-model as new gegelati container ref

--- container/dependencies_installation.sh
+++ container/dependencies_installation.sh
@@ -90,7 +90,7 @@ cd ../..
 echo "# Install Gegelati"
 git clone https://github.com/allpaul0/gegelati.git
 cd gegelati
-git checkout my-v2-develop
+git checkout my-v2-develop-model
 
 mkdir bin
 cd bin
