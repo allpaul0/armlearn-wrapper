6e67e7a28cc8d45a1a1c631ebd71a0fc765e5b9b
allpaul0 <paulallaireperso@gmail.com>
2026-06-15 15:05:47 +0200

    add mul only iset config to float dtype

--- src/instructions.cpp
+++ src/instructions.cpp
@@ -56,6 +56,11 @@ void fillInstructionSet(Instructions::Set& set, TrainingParameters params) {
             set.add(*(new Instructions::LambdaInstruction<double, double>(times, "$0 = $1 * $2;")));
             set.add(*(new Instructions::LambdaInstruction<double, double>(divide, "$0 = $1 / $2;")));
         }
+
+        if(!params.useInstrExpensiveArithmetic && params.useInstrZmmul){
+            set.add(*(new Instructions::LambdaInstruction<double, double>(times, "$0 = $1 * $2;")));
+        }
+
         if(params.useInstrComparison) {
             set.add(*(new Instructions::LambdaInstruction<double, double>(max, "$0 = fmaxf($1, $2);")));
         }
